4394882bb7e1cb66734eb793c600884c135b5cf0
pkristapovich <paulkristapovich@gmail.com>
2016-09-11 14:58:19 -0400

    Add files via upload

---
+++ vagrant/tournament/tournament.sql
@@ -0,0 +1,27 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+CREATE TABLE players ( id SERIAL PRIMARY KEY,
+                       name TEXT);
+
+CREATE TABLE matches ( mid SERIAL PRIMARY KEY,
+                       winner INT REFERENCES players(id),
+                       loser INT REFERENCES players(id));
+
+CREATE OR REPLACE VIEW STANDINGS AS
+    SELECT  id,
+            name,
+            SUM(CASE WHEN players.id = matches.winner THEN 1 ELSE 0 END) AS wins,
+            SUM(CASE WHEN players.id = matches.winner OR players.id = matches.loser THEN 1 ELSE 0 END) AS match_count
+    FROM players
+    LEFT JOIN matches
+    ON players.id = matches.winner OR players.id = matches.loser
+    GROUP BY players.id
+    ORDER BY wins DESC,
+             match_count ASC;
